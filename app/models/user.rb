c49060fb29ac491aa5bae8cd042c487a98ff60d4
Nantiwat Thongseng <nwatth@gmail.com>
2017-12-26 11:37:26 +0000

    using devise

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def apply_omniauth(auth)
+    update_attributes(
+      provider: auth.provider,
+      uid: auth.uid
+    )
+  end
+
+  def has_facebook_linked?
+    self.provider.present? && self.uid.present?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  authenticated :user do
+    root to: "home#index"
+  end
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+
+  resources :authentications, only: [:destroy]
 end
